48e2c0951496250a5e2c8f2d20b050e29fdd8d85
ani404 <chhotu.aniket@moneyforward.co.jp>
2024-02-05 17:06:40 +0900

    add application test

---
+++ src/test/kotlin/com/ani404/kotlinAlgorithms/KotlinAlgorithmsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ani404.kotlinAlgorithms
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinAlgorithmsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
