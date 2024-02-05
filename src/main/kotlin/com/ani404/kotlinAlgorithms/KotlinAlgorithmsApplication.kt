1e80bfc89356819edaaaaa2070cdfdf3b9932e3e
ani404 <chhotu.aniket@moneyforward.co.jp>
2024-02-05 17:06:34 +0900

    add application

---
+++ src/main/kotlin/com/ani404/kotlinAlgorithms/KotlinAlgorithmsApplication.kt
@@ -0,0 +1,11 @@
+package com.ani404.kotlinAlgorithms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinAlgorithmsApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinAlgorithmsApplication>(*args)
+}
